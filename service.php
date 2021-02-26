2f47e6f5cd113d1fe9c99e10e67b3e850423be69
anton90-ange <79692248+anton90-ange@users.noreply.github.com>
2021-02-26 16:16:22 +0700

    Add files via upload

---
+++ service.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="styleabout.css"> 
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h2 class="judul">Mywebsite</h2>
+            <ul>
+                <li><a href="index.html">Beranda</a>
+                <li><a href="home.php>Home</a>
+                <li><a href="about.php">About</a>
+                <li><a href="service.php">Services</a><li>
+            </ul>
+           </div>
+           <div class="horizontal">
+           </div>
+           <div class="hero">
+               <h1 class="judulhero">Subs to our newslatter</h1>
+               <div class="subscribe">
+                   <form action="">
+                       <input type="email" placeholder="Enter Email">
+                       <button type="submit" class="button_1">subscribe</button>
+                  </form>
+                  </div>
+                  </div>
+                  <div class="main">
+                      <h1 class="judul">Kosong</h1>
+                      <li>
+                          <h1>Website Design</h1>
+                          <p>Belajar Design, belajar editing, belajar banyak</p>
+
+                     </li>
+                     <li>
+                         <h1>Website Hosting></h1>
+                         <p>belajar membuat website, menjelajahi dunia perhostingan</p>
+                    </li>
+                    <li>
+                        <h1>Website Busini</h1>
+                        <p>saran mencari urasa, mengembangkan kemampuan, solusi menjadi sukses.></p>
+                    </li>
+                    </ul>
+                   </div>
+                   <div class="dark">
+                       <h3>Get A Quetec</h3>
+                       <form action="/action_page.php" class="from">
+                       <fieldset>
+                           <legend>personal information:</legend>
+                           first name:<br>
+                           <input type="text" name="lastname" value="Anton">><br><br>
+                           <input type="submit" value="submit">
+                       </fieldset>
+                      </form>
+                      </div>
+                       <div class="footber">
+                           <p class="copy">CopyRight2021. BY Ardiansyah</p>
+                       </div>
+                       </div>
+                       </body>
+                       </html>
+
